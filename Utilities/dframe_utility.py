48fd16879615a0e3c49857dde5f782627478eafe
Josh Villanueva <josh.villanueva@fortis.ph>
2025-11-22 17:12:25 +0800

    Add: helper functions to prevent code clutter

--- Utilities/dframe_utility.py
+++ Utilities/dframe_utility.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+import calendar
 
+now = datetime.now()
 
 #Get Borrowers list
 def get_borrowers_list(dataframe):
@@ -28,7 +31,6 @@ def get_bank_list(dataframe):
     return bank_list
 
 def get_total_amount_spent(dataframe,borrowers,bank=None):
-    # if isinstance(borrower,list):
     total_amount = []
     for borrower in borrowers:
         borrower_df = dataframe[dataframe["BORROWER"]==borrower.upper()]
@@ -47,3 +49,53 @@ def get_total_amount_spent(dataframe,borrowers,bank=None):
         total_amount.append(borrower_json)
     total_amount = sorted(total_amount, key=lambda x: x['Total'], reverse=True)
     return total_amount
+
+def get_monthly_due(dataframe,bank):
+    if bank:
+        dataframe = dataframe[dataframe["BANK"] == bank]
+
+    total = 0
+    for amount in dataframe["AMOUNT"]:
+        total += float(amount.replace(",",""))
+
+    return total
+
+
+# Create JSON for bank details
+def generate_bank_details(bank_list):
+    bank_details_list = []
+    for bank in bank_list:
+        if now.day in range(1,3):
+            month_int = now.month
+        else:
+            month_int = now.month + 1
+
+        if month_int > 12:
+            month_int -= 12
+
+        due_month = calendar.month_name[month_int].capitalize()
+        total_amount_string = f"Total Amount :color[(Due date: {due_month} 28])"
+
+        if bank == 'EASTWEST':
+            total_amount_string = total_amount_string.replace(':color',":violet")
+        elif bank == 'RCBC':
+            total_amount_string = total_amount_string.replace(':color',":blue")
+        elif bank == 'BPI':
+            if now.day in range(1,15):
+                month_int = now.month
+            else:
+                month_int = now.month + 1
+
+            if month_int > 12:
+                month_int -= 12
+
+            due_month = calendar.month_name[month_int].capitalize()
+            total_amount_string = f"Total Amount :red[(Due date: {due_month.capitalize()} 03])"
+
+        bank_details = {
+            "bank": bank,
+            "total_amount_string": total_amount_string
+        }
+        bank_details_list.append(bank_details)
+
+    return bank_details_list
