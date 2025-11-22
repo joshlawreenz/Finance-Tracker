ea1cfd3af7b4e833fb377c0accfd12ed79b8f87b
Josh Villanueva <josh.villanueva@fortis.ph>
2025-11-23 04:08:07 +0800

    Add: ability to process installment transactions and compute total correctly

--- finance_trackerv2.py
+++ finance_trackerv2.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 import st_yled
 import time
 
@@ -63,11 +64,17 @@ df = df[::-1]
 
 borrowers = default_borrowers = get_borrowers_list(df)
 # bank_list = get_bank_list(df)
+
+months_capitalized = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+]
+
 bank_list = ['EASTWEST','RCBC', 'BPI']
 bank_list_colored = [':violet[EASTWEST]',':blue[RCBC]',':red[BPI]']
 
 # Get Total Amount Spent
-sidebar_total_amount = get_total_amount_spent(df,borrowers)
+sidebar_total_amount = get_total_amount_spent(df,borrowers=borrowers)
 
 if "filter_name" not in st.session_state:
     borrowers = [i.upper() for i in borrowers]
@@ -81,7 +88,11 @@ eastwest, rcbc, bpi = st.tabs(bank_list_colored,default=':violet[EASTWEST]')
 tab_list = [eastwest,rcbc,bpi]
 
 df_display = df.copy()
-df_display.drop(hide_columns, inplace=True, axis=1)
+
+try:
+    df_display.drop(hide_columns, inplace=True, axis=1)
+except KeyError:
+    pass
 
 # Change date format of display df
 df_display["DATE"] = pd.to_datetime(df["DATE"])
@@ -99,7 +110,6 @@ for tab in tab_list:
         bank = bank_list[index]
         breakdown = get_total_amount_spent(df_display,borrowers,bank)
         bank_df = df_display[df_display["BANK"] == bank]
-        monthly_due = get_monthly_due(df_display,bank)
 
         logo_col,breakdown_col,totalamount_col = st.columns([0.25,0.25,0.34],vertical_alignment='center')
 
@@ -108,25 +118,105 @@ for tab in tab_list:
             display_logo(bank)
             default_borrowers = [i.capitalize() for i in default_borrowers]
 
-            # Allows User filtering
-            filter_options = st.pills(
-                "Filter", 
-                default_borrowers, 
-                selection_mode="multi",
-                key=bank,
-                default=default_borrowers,
-                label_visibility="hidden"
-                )
-            borrowers = filter_options
+            statementdate_col,filter_col,  = st.columns(2,vertical_alignment='top')
+
+            with filter_col:
+                # Allows User filtering
+                filter_options = st.pills(
+                    "Select borrowers to display", 
+                    default_borrowers, 
+                    selection_mode="multi",
+                    key=bank,
+                    default=default_borrowers,
+                    label_visibility="visible"
+                    )
+                borrowers = filter_options
+                
+            with statementdate_col:
+                statement_month = st.selectbox('Select Statement Period',label_visibility='visible',options=months_capitalized,key=bank+'statement',index=now.month-1)
+
+        st.subheader("Transactions")
+        # Apply borrower filter to dataframe
+        bank_borrowers = [i.upper() for i in borrowers]
+        bank_df = bank_df[bank_df["BORROWER"].isin(bank_borrowers)]
+
+        # Sort transactions based on date
+        bank_df["DATE"] = pd.to_datetime(bank_df["DATE"])
+        bank_df = bank_df.sort_values("DATE", ascending=False)
+        bank_df["DATE"] = bank_df["DATE"].dt.strftime("%B %d, %Y")
+
+        # Filters to current cutoff
+        current_year = now.strftime("%Y").upper()
+
+        # EASTWEST & RCBC
+        if statement_month:
+            # statement_month
+            month_int = datetime.strptime(f"{statement_month} 1, 1900",'%B %d, %Y').month
+        if now.day in range(1,3):
+            pass
+        else:
+            month_int = month_int + 1
+
+        if bank in ['EASTWEST','RCBC']:
+            if month_int > 12:
+                month_res = month_int - 12
+                cutoff_enddate = datetime(int(current_year), abs(int(month_res)), 2) + relativedelta(years=1)
+                cutoff_startdate = cutoff_enddate - relativedelta(months=1) + relativedelta(days=1)
+            else:
+                cutoff_enddate = datetime(int(current_year), int(month_int), 2)
+                cutoff_startdate = cutoff_enddate - relativedelta(months=1) + relativedelta(days=1)
+        elif bank == 'BPI':
+            if month_int > 12:
+                month_res = month_int - 12
+                cutoff_enddate = datetime(int(current_year), abs(int(month_res)), 13) + relativedelta(years=1)
+                cutoff_startdate = cutoff_enddate - relativedelta(months=1) + relativedelta(days=1)
+            else:
+                cutoff_enddate = datetime(int(current_year), int(month_int), 13)
+                cutoff_startdate = cutoff_enddate - relativedelta(months=1) + relativedelta(days=1)
+
+
+        bank_df["DATE"] = pd.to_datetime(bank_df["DATE"])
+        bank_df = bank_df[(bank_df["DATE"] >= cutoff_startdate) & (bank_df["DATE"] <= cutoff_enddate)]
+        bank_df["DATE"] = bank_df["DATE"].dt.strftime("%B %d, %Y")
+
+        # Reduces date redundancy
+        bank_df["DATE"] = bank_df["DATE"].mask(bank_df["DATE"].duplicated(), "")
+
+        # Dataframe Config
+        bankdf_config = {
+                        "DATE": st.column_config.Column(
+                            width="small"
+                        ),
+                        "DESCRIPTION": st.column_config.Column(
+                            width="large"
+                        ),
+                        "AMOUNT": st.column_config.NumberColumn(
+                            format="₱ %.2f",
+                            width="medium"
+                        )
+                    }
+        
+        st.dataframe(bank_df,width="stretch",hide_index=True, column_config=bankdf_config)
 
         # Breakdown Column
         with breakdown_col:
             st.subheader("Breakdown")
-            st.dataframe(breakdown,hide_index=True)
+            breakdown = get_total_amount_spent(df_display,borrowers,bank,start=cutoff_startdate,end=cutoff_enddate)
+            # Dataframe Config
+            breakdown_config = {
+                            "Total": st.column_config.NumberColumn(
+                                format="₱ %.2f",
+                                width="medium"
+                            )
+                        }
+            st.dataframe(breakdown,hide_index=True,column_config=breakdown_config)
 
         # Transaction Metric (WIP)
         with totalamount_col:
             string = bank_details_list[index].get('total_amount_string')
+            monthly_due = get_monthly_due(df_display,bank,start=cutoff_startdate,end=cutoff_enddate)
+
+
             st.metric(string, 
                         "₱" + str(f"{monthly_due:.2f}"),
                         # abs(monthly_due/2 - monthly_due),
@@ -136,43 +226,11 @@ for tab in tab_list:
                         border=True,
                         )
 
-        st.subheader("Transactions")
-        # Filters borrowers
-        bank_borrowers = [i.upper() for i in borrowers]
-        bank_df = bank_df[bank_df["BORROWER"].isin(bank_borrowers)]
-
-        # Sort transactions based on date
-        bank_df["DATE"] = pd.to_datetime(bank_df["DATE"])
-        bank_df = bank_df.sort_values("DATE", ascending=False)
-        bank_df["DATE"] = bank_df["DATE"].dt.strftime("%B %d, %Y")
-
-        # Reduces date redundancy
-        bank_df["DATE"] = bank_df["DATE"].mask(bank_df["DATE"].duplicated(), "")
-
-        st.dataframe(bank_df, width="stretch", hide_index=True)
-
         index += 1
         translist_col,col2,col3,transmodifier_col = st.columns([0.5,0.15,0.5,0.15],vertical_alignment="center",border=False)
-        # Transaction List (Dataframe)
-        # with translist_col:
-        #     st.subheader("Transactions")
-        #     # Filters borrowers
-        #     bank_borrowers = [i.upper() for i in borrowers]
-        #     bank_df = bank_df[bank_df["BORROWER"].isin(bank_borrowers)]
 
-        #     # Sort transactions based on date
-        #     bank_df["DATE"] = pd.to_datetime(bank_df["DATE"])
-        #     bank_df = bank_df.sort_values("DATE", ascending=False)
-        #     bank_df["DATE"] = bank_df["DATE"].dt.strftime("%B %d, %Y")
-
-        #     # Reduces date redundancy
-        #     bank_df["DATE"] = bank_df["DATE"].mask(bank_df["DATE"].duplicated(), "")
-
-        #     st.dataframe(bank_df, width="stretch", hide_index=True)
-
-        #     index += 1
-
-        # Transaction Modifiers
+        ## Transaction Modifiers
+        # Add Transaction Modal
         with transmodifier_col:
             @st.dialog("Add Transaction",on_dismiss="rerun")
             def add_transaction():
@@ -192,17 +250,20 @@ for tab in tab_list:
                 
                 # User and Bank Column
                 user_col, bank_col = st.columns(2)
-                transaction_borrower = user_col.multiselect('Card User/s',options=default_borrowers)
+                transaction_borrower = user_col.multiselect('Card User/s',options=default_borrowers,default="Josh")
                 bank_selection = bank_col.selectbox('Bank',options=bank_list)
                 
                 # Text Columns
-                description = st.text_input("Description")
-                
-                remarks = st.text_area('Remarks')
+                description = st.text_input("Description",max_chars=50)
+                remarks_check = st.checkbox('Remarks')
+
+                remarks = None
+                if remarks_check:
+                    remarks = st.text_area('Remarks',label_visibility="collapsed",max_chars=100)
                 
                 # Installment Columns
                 installment_check_col, installment_drop = st.columns(2)
-                installment_check = installment_check_col.checkbox('Installment?')
+                installment_check = installment_check_col.checkbox('Installment terms')
                 installment_selection = None
                 if installment_check:
                     installment_selection = installment_drop.selectbox(
@@ -217,42 +278,99 @@ for tab in tab_list:
 
                 # Submit button Column
                 if button_col.button("Submit"):
-                    with st.spinner("Submitting...", show_time=False):
-                        # Generates multiple entries based on installment terms
-                        if installment_check:
-                            row_list = []
-                            monthly_term, x = installment_selection.split(" ")
-                            divided_amount = amount / float(monthly_term)
+                    # Makes user fill-up required fields
+                    if not (amount 
+                            and description 
+                            and bank_selection 
+                            and date_of_transaction
+                            and transaction_borrower):
+                        st.warning('Please fill up required fields.')
+                    else:
+                        # Spinner loader
+                        with st.spinner("Submitting...", show_time=False):
+                            # Generates multiple entries based on installment terms
+                            file_path = "output.csv"
+                            write_header = not os.path.exists(file_path) or os.path.getsize(file_path) == 0
 
-                            for i in range(int(monthly_term)):
-                                suffix = f" ({i+1}/{monthly_term})"
-                                new_row = {
-                                    "DATE": date_of_transaction,
-                                    "DESCRIPTION": description + suffix,
-                                    "BANK": bank_selection.upper(),
-                                    "BORROWER": transaction_borrower,
-                                    "AMOUNT": int(divided_amount),
-                                    "INSTALLMENT": installment_selection,
-                                    "REMARKS": remarks
-                                }
+                            # transaction_borrower = ",".join(transaction_borrower).upper()
+                            if installment_check:
+                                row_list = []
+                                monthly_term, x = installment_selection.split(" ")
+                                divided_amount = amount / float(monthly_term)
+                                initial_date_of_transaction = date_of_transaction
 
-                                row_list.append(new_row)
-                        else:
-                            new_row = {
-                                "DATE": date_of_transaction,
-                                "DESCRIPTION": description,
-                                "BANK": bank_selection.upper(),
-                                "BORROWER": transaction_borrower,
-                                "AMOUNT": amount,
-                                "INSTALLMENT": installment_selection,
-                                "REMARKS": remarks
-                            }
-                        # with st.spinner("Submitting...", show_time=False):
-                        time.sleep(3)
-                    st.success('Transaction submitted!', icon="✅")
-                    row_list
-                    time.sleep(1.5)
-                    # st.rerun()
+                                # If multiple users are selected, loop the users
+                                if len(transaction_borrower) > 1:
+                                    divided_amount = divided_amount/len(transaction_borrower)
+                                    for borrower in transaction_borrower:
+                                        date = initial_date_of_transaction
+                                        for i in range(int(monthly_term)):
+                                            suffix = f" ({i+1}/{monthly_term})"
+                                            new_row = {
+                                                "DATE": date,
+                                                "DESCRIPTION": description + suffix,
+                                                "BANK": bank_selection.upper(),
+                                                "BORROWER": borrower.upper(),
+                                                "AMOUNT": float(divided_amount),
+                                                "INSTALLMENT": installment_selection,
+                                                "REMARKS": remarks
+                                            }
+                                            # Adds 1 month to date per installment
+                                            date = date + relativedelta(months=1)
+                                            row_list.append(new_row)
+                                else:
+                                    for i in range(int(monthly_term)):
+                                        suffix = f" ({i+1}/{monthly_term})"
+                                        new_row = {
+                                            "DATE": date_of_transaction,
+                                            "DESCRIPTION": description + suffix,
+                                            "BANK": bank_selection.upper(),
+                                            "BORROWER": transaction_borrower[0].upper(),
+                                            "AMOUNT": float(divided_amount),
+                                            "INSTALLMENT": installment_selection,
+                                            "REMARKS": remarks
+                                        }
+                                        # Adds 1 month to date per installment
+                                        date_of_transaction = date_of_transaction + relativedelta(months=1)
+                                        row_list.append(new_row)
+
+                                df_new = pd.DataFrame(row_list)
+                                
+                                write_header = not os.path.exists(file_path) or os.path.getsize(file_path) == 0
+                                df_new.to_csv(file_path, mode="a",header=write_header,index=False)
+                            else:
+                                if len(transaction_borrower) > 1:
+                                    transaction_list = []
+                                    amount = amount/len(transaction_borrower)
+                                    for borrower in transaction_borrower:
+                                        new_row = {
+                                            "DATE": date_of_transaction,
+                                            "DESCRIPTION": description,
+                                            "BANK": bank_selection.upper(),
+                                            "BORROWER": borrower.upper(),
+                                            "AMOUNT": float(amount),
+                                            "INSTALLMENT": None,
+                                            "REMARKS": remarks
+                                        }
+                                        transaction_list.append(transaction_list)
+                                else:
+                                    new_row = {
+                                        "DATE": date_of_transaction,
+                                        "DESCRIPTION": description,
+                                        "BANK": bank_selection.upper(),
+                                        "BORROWER": transaction_borrower[0].upper(),
+                                        "AMOUNT": float(amount),
+                                        "INSTALLMENT": None,
+                                        "REMARKS": remarks
+                                    }
+                                    transaction_list = [new_row]
+                                df_new = pd.DataFrame(transaction_list)
+                                df_new.to_csv(file_path, mode="a",header=write_header,index=False)
+
+                            # with st.spinner("Submitting...", show_time=False):
+                            time.sleep(1.2)
+                        st.success('Transaction submitted!', icon="✅")
+                        st.rerun()
                 
             if st.button("Add Transaction",key=bank+'add'):
                 add_transaction()
